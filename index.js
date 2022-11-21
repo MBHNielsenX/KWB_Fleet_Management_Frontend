6b046f606530f98111ed0d213f307e4abf4d2e94
IfMalthereturnawesome <malt996b@stud.kea.dk>
2022-11-21 20:22:25 +0100

    Added Sidebar and favicons

---
+++ index.js
@@ -0,0 +1,14 @@
+// EDIT THIS FILE - Bare et template fra car
+
+import "https://unpkg.com/navigo"  //Will create the global Navigo object used below
+import {
+  loadHtml,
+  adjustForMissingHash,
+  setActiveLink,
+  renderTemplate,
+
+} from "./utils.js"
+
+import "./src/js/sidenav.js"
+
+
