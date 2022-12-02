19af64c0512d5ddd0663dd20515413c189730003
JensLegarthRyge <jens.legarth.ryge@gmail.com>
2022-12-02 11:26:35 +0100

    Reworked to allow for displaying of amount of colorMixes pr. specificCarmodel

--- src/pages/all-brands/allBrands.js
+++ src/pages/all-brands/allBrands.js
@@ -5,7 +5,7 @@ let specificCarModels = [];
 export async function initAllBrands(){
 
     try{
-        specificCarModels = await fetch(SERVER_URL+"/specific-car-model")
+        specificCarModels = await fetch(SERVER_URL+"/specific-car-model/all-with-color-mix-count")
             .then(res => res.json())
     } catch (e){
         console.error(e)
@@ -16,6 +16,7 @@ export async function initAllBrands(){
             <td>${specificCarModel.brand}</td>
             <td>${specificCarModel.model}</td>
             <td>${specificCarModel.modelYear}</td>
+            <td>${specificCarModel.colorMixAmounts}</td>
         </tr>`)
         .join("")
 
