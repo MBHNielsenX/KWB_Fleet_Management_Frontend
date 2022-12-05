296bf0087e9ac41b539104b67a151d7db3891c66
IfMalthereturnawesome <malt996b@stud.kea.dk>
2022-12-05 16:06:26 +0100

    Added Create User Site

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import "https://unpkg.com/navigo"  //Will create the global Navigo object used below
 import {loadHtml, adjustForMissingHash, setActiveLink, renderTemplate} from "./utils.js"
 
+
+
 import { initCreateBrand } from "./src/pages/create-brand/createBrand.js"
 import { initAllBrands } from "./src/pages/all-brands/allBrands.js";
 import { initLogin } from "./src/pages/login/login.js";
@@ -17,6 +19,7 @@ import { initAllUserLogin } from "./src/pages/createLogins/allUsers/allUsersLogi
 
 window.addEventListener("load", async () => {
   const templateHome = await loadHtml("./src/pages/home/home.html")
+  const templateCreateUser = await loadHtml("./src/pages/createLogins/createUser/createUser.html")
   const templateCreateBrand = await loadHtml("./src/pages/create-brand/create-brand.html")
   const templateAllBrands = await loadHtml("./src/pages/all-brands/all-brands.html")
   const templateLogin = await loadHtml("./src/pages/login/login.html")
@@ -54,19 +57,20 @@ window.addEventListener("load", async () => {
         renderTemplate(templateAllBrands, "content")
         initAllBrands()
       },
-      "/create/login/admin": () => {
+      "/users/create-user": () => renderTemplate(templateCreateUser, "content"),
+      "/users/create-admin": () => {
         renderTemplate(templateCreateAdminLogin, "content")
         initCreateAdminLogin()
       },
-      "/create/login/buyer": () => {
+      "/users/create-buyer": () => {
         renderTemplate(templateCreateBuyerLogin, "content")
         initCreateBuyerLogin()
       },
-      "/create/login/leaser": () => {
+      "/users/create-leaser": () => {
         renderTemplate(templateLeaserLogin, "content")
         initCreateLeaserLogin()
       },
-      "/create/login/economy": () => {
+      "/users/create-economy": () => {
         renderTemplate(templateEconomyLogin, "content")
         initCreateEconomyLogin()
       },
