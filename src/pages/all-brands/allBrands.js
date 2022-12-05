9093ef66ecf5cccb14ebde81003af4b4b3e6bbf2
JensLegarthRyge <jens.legarth.ryge@gmail.com>
2022-12-05 18:44:36 +0100

    + support for redirecting to specific page for color mix adding + deletion of brand + redirection to editing site

--- src/pages/all-brands/allBrands.js
+++ src/pages/all-brands/allBrands.js
@@ -1,6 +1,8 @@
 import {SERVER_URL} from "../../../settings.js";
+import {checkRoleAdmin, checkTokenDelete, checkTokenGet} from "../../js/loginSettings.js";
+
 let url = SERVER_URL+"/specific-car-model/all-with-color-mix-count"
-import { checkRoleAdmin, checkTokenGet } from "../../js/loginSettings.js";
+let deleteUrl = SERVER_URL+"/specific-car-model/"
 
 let specificCarModels = [];
 
@@ -8,11 +10,17 @@ export async function initAllBrands(){
     checkRoleAdmin()
     document.getElementById("table-body").onclick = (element) =>{
         let id = element.target.id
-        if (id.startsWith("btn-add-color-mix-")){
+        if (id.startsWith("btn-add-color-mix-") || id.startsWith("link-view-color-mixes")){
             addColorMixRedirect(id)
+        } else if (id.startsWith("btn-kebab-menu-")){
+            displayKebabMenu(id)
+        } else if (id.startsWith("link-delete-brand-")){
+            deleteBrand(id)
+        } else if (id.startsWith("link-edit-brand-")){
+            editBrandRedirect(id)
         }
-
     }
+
     try{
         specificCarModels = await fetch(url, await checkTokenGet())
             .then(res => res.json())
@@ -27,14 +35,43 @@ export async function initAllBrands(){
             <td>${specificCarModel.modelYear}</td>
             <td>${specificCarModel.colorMixAmounts}</td>
             <td><button id="btn-add-color-mix-${specificCarModel.id}">Add Colormix</button></td>
-            <td><button id="${specificCarModel.id}">Kebab menu</button></td>
+            <td><button id="btn-kebab-menu-${specificCarModel.id}">...</button></td>
+            <ul id="kebab-menu-${specificCarModel.id}"></ul>
         </tr>`)
         .join("")
 
     document.getElementById("table-body").innerHTML=rows
 }
 
-async function addColorMixRedirect(id){
+async function getId(id){
     id = id.split('-')
-    router.navigate(`color-mix?id=${id}`)
+    return id[id.length-1]
+}
+
+async function addColorMixRedirect(id){
+    router.navigate(`color-mix?id=${await getId(id)}`)
+}
+
+async function editBrandRedirect(id){
+    router.navigate(`edit-brand?id=${await getId(id)}`)
+}
+
+async function displayKebabMenu(id){
+    document.getElementById(id).innerHTML = `
+    <li><a id="link-view-color-mixes-${id}"">View Color mixes</a></li>
+    <li><a id="link-edit-brand-${id}">Edit Brand</a></li>
+    <li><a id="link-delete-brand-${id}">Delete Brand</a></li>
+    `;
+}
+
+async function deleteBrand(id){
+    const confirmDeletion = confirm("Are you sure you wish to delete brand with id: " + await getId(id)+"?");
+    try {
+        if (confirmDeletion === true) {
+            await fetch(deleteUrl + await getId(id), await checkTokenDelete())
+            location.reload()
+        }
+    } catch (e){
+        console.log(e)
+    }
 }
