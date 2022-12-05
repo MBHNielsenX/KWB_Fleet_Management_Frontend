f2a6cb9781fae599e7badd2bd7bd6f15947f4bbd
Mojens <moha135h@stud.kea.dk>
2022-12-05 15:30:15 +0100

    button for activate and deactivate

--- src/pages/create-blacklist/createblackList.js
+++ src/pages/create-blacklist/createblackList.js
@@ -9,18 +9,18 @@ export async function initCreateBlackList() {
     checkRoleAdmin();
     getAllBlackList();
     document.getElementById("blacklist-create-btn").onclick = createBlackList;
+    document.getElementById("blacklist-delete-btn").onclick = deleteBlackList;
 }
 
 
 async function getAllBlackList() {
     const blackListRes = await fetch(URL, await checkTokenGet()).then(res => handleHttpErrors(res));
     let ul = document.getElementById("list-of-blacklist");
-    let li = document.createElement("li");
+    ul.innerHTML = ` `;
     blackListRes.map(blackList => {
-        li.innerHTML = blackList.vinNumber+ ", STATUS: " + blackList.status;
-        ul.appendChild(li);
+        let activeOrNo = blackList.status == "ACTIVE" ? "activated" : "deactivated";
+        ul.innerHTML += `<li>${blackList.vinNumber}, STATUS: ${blackList.status} <button id = "${blackList.vinNumber}-${activeOrNo}">${activeOrNo}</button></li>`
     });
-
 }
 
 async function createBlackList() {
@@ -29,7 +29,14 @@ async function createBlackList() {
         vinNumber: vinNumber.value
     }
 
-    const blackListRes = await fetch(URL, await checkTokenPost(obj));
-    getAllBlackList();
+  await fetch(URL, await checkTokenPost(obj)).then(res => handleHttpErrors(res));
+}
+
+async function deleteBlackList() {
+    const vinNumber = document.getElementById("vin-number-input-blacklist")
+
+
+    await fetch(URL + "/" + vinNumber.value, await checkTokenDelete()).then(res => handleHttpErrors(res));
 
 }
+
