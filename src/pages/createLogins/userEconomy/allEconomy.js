38deff91b3e01c33dda86a4e7f8e88433942c31d
IfMalthereturnawesome <malt996b@stud.kea.dk>
2022-12-06 18:31:03 +0100

    Add 3 dot modal design

--- src/pages/createLogins/userEconomy/allEconomy.js
+++ src/pages/createLogins/userEconomy/allEconomy.js
@@ -11,7 +11,7 @@ export async function initGetAllEconomyUsers() {
     document.getElementById("table-body").onclick = (element) => {
         let id = element.target.id
         if (id.startsWith("btn-edit-economy-user-")) {
-            editLeaserUserRedirect(id)
+            editEconomyUserRedirect(id)
         }
 
     }
@@ -24,7 +24,7 @@ export async function initGetAllEconomyUsers() {
     }
 
     const rows = economyUsers.map(economyUser =>
-        `<tr>      
+        `<tr style="opacity: 1">      
          <td>${economyUser.userName}</td>
            <td>${economyUser.firstName} ${economyUser.lastName}</td>
         <td><a href="tel:${economyUser.phoneNumber}">${economyUser.phoneNumber}</a </td>
@@ -32,12 +32,47 @@ export async function initGetAllEconomyUsers() {
         <td>${economyUser.ownership}</td>
         <td>${economyUser.status}</td>
                 
+          
             <td>
-                <button id="${economyUser.id}-column-id" type="button"  class="primary-button" data-bs-toggle="modal" data-bs-target="#exampleModal">Edit</button> 
+             <ul data-bs-toggle="modal" data-bs-target="#exampleModal" class="three-dots" >
+                                    <li id="${economyUser.id}-column-id"  class="three-dots__dot"></li>
+                                    <li id="${economyUser.id}-column-id"  class="three-dots__dot"></li>
+                                    <li id="${economyUser.id}-column-id"  class="three-dots__dot"></li>
+             </ul>
             </td>
         </tr>`
     )
+
     document.getElementById("table-body").innerHTML = rows.join("")
+
+
+    document.getElementById("table-body").onclick = (element) => {
+        let id = element.target.id
+        if (id.endsWith("-column-id")) {
+            // the clicked row
+            let row = document.getElementById(id).closest("tr")
+            // the other rows
+            let rows = document.getElementById("table-body").children
+            for (let i = 0; i < rows.length; i++) {
+                if (rows[i] !== row) {
+                    rows[i].style.opacity = "0.5"
+                }
+            }
+        }
+
+    }
+
+
+    document.getElementById("exampleModal").addEventListener("hidden.bs.modal", () => {
+        let rows = document.getElementById("table-body").children
+        for (let i = 0; i < rows.length; i++) {
+            rows[i].style.opacity = "1"
+        }
+
+    }
+    )
+
+
 
 }
 
