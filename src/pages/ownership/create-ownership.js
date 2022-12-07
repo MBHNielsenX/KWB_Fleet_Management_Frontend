410e2d4a42788cf7bc741340af237464e06696f9
ChineseThunder <simo09j9@stud.kea.dk>
2022-12-07 10:41:42 +0100

    create i ownership

--- src/pages/ownership/create-ownership.js
+++ src/pages/ownership/create-ownership.js
@@ -8,9 +8,23 @@ let router;
 export function initCreateOwnership(navigoRouter) {
     checkRoleAdmin();
     router = navigoRouter
-    document.getElementById("btn-create-ownership").onclick = addOwnership;
+    document.getElementById("btn-create-ownership").onclick = addOwnership
 }
 
 async function addOwnership() {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
 
+    var raw = JSON.stringify({
+        "name": document.getElementById("name").value,
+        "abbreviation": document.getElementById("abbreviation").value
+    });
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
 }
+
