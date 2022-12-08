1d7041b80f8938387fae8bdeeaa880d80fbb326f
IfMalthereturnawesome <malt996b@stud.kea.dk>
2022-12-08 11:53:30 +0100

    Add Import rowHighlight Function

--- src/js/modulLoad.js
+++ src/js/modulLoad.js
@@ -1,26 +1,28 @@
 
-document.getElementById("table-body").onclick = (element) => {
-    let id = element.target.id
-    if (id.endsWith("-column-id")) {
-        // the clicked row
-        let row = document.getElementById(id).closest("tr")
-        // the other rows
-        let rows = document.getElementById("table-body").children
-        for (let i = 0; i < rows.length; i++) {
-            if (rows[i] !== row) {
-                rows[i].style.opacity = "0.5"
+export function rowHighlight() {
+    document.getElementById("table-body").onclick = (element) => {
+        let id = element.target.id
+        if (id.endsWith("-column-id")) {
+            // the clicked row
+            let row = document.getElementById(id).closest("tr")
+            // the other rows
+            let rows = document.getElementById("table-body").children
+            for (let i = 0; i < rows.length; i++) {
+                if (rows[i] !== row) {
+                    rows[i].style.opacity = "0.5"
+                }
             }
         }
+
     }
 
-}
 
+    document.getElementById("exampleModal").addEventListener("hidden.bs.modal", () => {
+            let rows = document.getElementById("table-body").children
+            for (let i = 0; i < rows.length; i++) {
+                rows[i].style.opacity = "1"
+            }
 
-document.getElementById("exampleModal").addEventListener("hidden.bs.modal", () => {
-        let rows = document.getElementById("table-body").children
-        for (let i = 0; i < rows.length; i++) {
-            rows[i].style.opacity = "1"
         }
-
-    }
-)
+    )
+}
